9f9ae871e10c07a00b31c4b6cc1bb791bf0f9ceb
Tamara Elaye <tamaraelaye@gmail.com>
2024-11-02 10:07:38 +0100

    style: declare the primary and secondary colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,11 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: "#001940",
+        secondary: "#dd3333",
+        background: '#f6f6f6',
+      },
     },
   },
   plugins: [],
