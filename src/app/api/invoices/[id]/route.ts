c897589729d2d90ccec32dc6e0463dd1cf2bca09
Tamara Elaye <tamaraelaye@gmail.com>
2024-11-04 23:25:24 +0100

    chore: update api invoice api

--- src/app/api/invoices/[id]/route.ts
+++ src/app/api/invoices/[id]/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
-import dbConnect from "@/lib/db";
-import Invoice from "@/model/invoice";
+import dbConnect from '../../../../lib/db';
+import Invoice from '../../../../model/invoice';
 
 // GET: Fetch single invoice
 export async function GET(
