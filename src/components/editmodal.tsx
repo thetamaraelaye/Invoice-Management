359c360c0cb3b3f90d1becbfc75d0cdff091d2e9
Tamara Elaye <tamaraelaye@gmail.com>
2024-11-05 04:06:41 +0100

    style: add more styling to the edit modal

--- src/components/editmodal.tsx
+++ src/components/editmodal.tsx
@@ -1,6 +1,6 @@
 // components/EditModal.tsx
-"use client"
-import { Dialog, Transition } from "@headlessui/react";
+"use client";
+import { Dialog, DialogPanel, Transition } from "@headlessui/react";
 import React, { Fragment, useState } from "react";
 
 interface EditModalProps {
@@ -30,37 +30,65 @@ const EditModal: React.FC<EditModalProps> = ({
   return (
     <Transition appear show={isOpen} as={Fragment}>
       <Dialog as="div" className="relative z-10" onClose={closeModal}>
-        <Transition.Child as={Fragment}>
+        <Transition as={Fragment}>
           <div className="fixed inset-0 bg-black bg-opacity-25" />
-        </Transition.Child>
+        </Transition>
         <div className="fixed inset-0 overflow-y-auto">
           <div className="flex items-center justify-center min-h-full">
-            <Dialog.Panel className="w-full max-w-md p-6 bg-white rounded">
-              <Dialog.Title>Edit Invoice</Dialog.Title>
-              <form onSubmit={handleSubmit}>
-                <input
-                  type="text"
-                  value={customerName}
-                  onChange={(e) => setCustomerName(e.target.value)}
-                  placeholder="Customer Name"
-                  required
-                />
-                <input
-                  type="number"
-                  value={amount}
-                  onChange={(e) => setAmount(Number(e.target.value))}
-                  placeholder="Amount"
-                  required
-                />
-                <input
-                  type="date"
-                  value={dueDate}
-                  onChange={(e) => setDueDate(e.target.value)}
-                  required
-                />
-                <button type="submit">Save</button>
+            <DialogPanel className="w-full max-w-md p-6 bg-white rounded">
+              <h2 className="text-lg font-bold text-primary">Edit Invoice</h2>
+              <form
+                onSubmit={handleSubmit}
+                className="space-y-4 flex flex-col p-4"
+              >
+                <div>
+                  <label
+                    htmlFor={customerName}
+                    className="text-sm font-medium text-gray-600"
+                  >
+                    Customer Name
+                  </label>
+                  <input
+                    type="text"
+                    value={customerName}
+                    onChange={(e) => setCustomerName(e.target.value)}
+                    placeholder="Customer Name"
+                    required
+                    className="w-full rounded-lg border border-gray-200 bg-gray-100 px-3 py-3 text-sm ring-0 focus:outline-none"
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor={amount}
+                    className="text-sm font-medium text-gray-600"
+                  >
+                    Amount
+                  </label>
+                  <input
+                    type="number"
+                    value={amount}
+                    onChange={(e) => setAmount(Number(e.target.value))}
+                    placeholder="Amount"
+                    required
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor={dueDate}
+                    className="text-sm font-medium text-gray-600"
+                  >
+                    Due Date
+                  </label>
+                  <input
+                    type="date"
+                    value={dueDate}
+                    onChange={(e) => setDueDate(e.target.value)}
+                    required
+                  />
+                </div>
+                <button type="submit" className="bg-primary hover:bg-primary-dark focus:bg-primary text-white font-semibold rounded-lg transition-colors duration-300 py-2 px-3 text-right">Save</button>
               </form>
-            </Dialog.Panel>
+            </DialogPanel>
           </div>
         </div>
       </Dialog>
