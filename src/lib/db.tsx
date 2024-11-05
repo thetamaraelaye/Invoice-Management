c397c187c07e3f80e2cab3eca2d71beec6278e81
Tamara Elaye <tamaraelaye@gmail.com>
2024-11-05 09:34:10 +0100

    chore: increase timeout for db connection

--- src/lib/db.tsx
+++ src/lib/db.tsx
@@ -1,12 +1,13 @@
 import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGOD_URI || ''; // Add your MongoDB URI to .env
-
 const dbConnect = async () => {
-  if (mongoose.connection.readyState >= 1) {
-    return;
-  }
-  return mongoose.connect(MONGODB_URI);
+  if (mongoose.connection.readyState >= 1) return;
+
+  return mongoose.connect(MONGODB_URI, {
+    serverSelectionTimeoutMS: 30000, // 30 seconds
+    socketTimeoutMS: 60000, // 60 seconds
+  });
 };
 
 export default dbConnect;
